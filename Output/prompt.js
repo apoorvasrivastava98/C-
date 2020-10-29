cc4fc44cddcb3a00ff2ffea3f649aa8a330bed91
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-10-29 10:58:48 +0530

    Create prompt.js

---
+++ Output/prompt.js
@@ -0,0 +1,17 @@
+//http://codepen.io/ColleenEMc/pen/pgvwVo
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <title>Prompt</title>
+</head>
+
+<body>
+  
+	  <script>
+   prompt("What is your name?");
+	  </script>
+  <p>This program just keeps popping up prompts.  It is both useless and annoying, but it shows you how to use prompt.</p>
+</body>
+</html>
