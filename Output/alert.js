d2178a568fe83fb19abfd7e185bb4c67526e5caa
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-10-29 10:57:31 +0530

    Create alert.js

---
+++ Output/alert.js
@@ -0,0 +1,13 @@
+// http://codepen.io/ColleenEMc/pen/jbYEEW
+
+<body>
+  Hi There<br/>
+	  <script>
+		  alert("Test")
+	  </script>
+	  <noscript>
+		  Your browser doesn't support or has disabled JavaScript</noscript>
+  
+  Good-bye
+</body>
+</html>
